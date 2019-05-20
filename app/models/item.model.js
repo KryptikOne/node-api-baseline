d7c42eae59558098229d285c5d469b0e14f1109d
Jason Saba <KryptikOne@users.noreply.github.com>
2019-05-20 00:54:02 -0500

    Initial configuration and files

---
+++ app/models/item.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const ItemSchema = mongoose.Schema({
+    title: String,
+    content: String
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Item', ItemSchema)
